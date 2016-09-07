1d005586a304515b17e963fd0bc7893c682c2428
Dinesh Patra <dineshpatra28@gmail.com>
2016-09-07 21:27:05 +0530

    removing the wsgi

--- python-sidecar/sidecar/app.py
+++ python-sidecar/sidecar/app.py
@@ -1,23 +1,111 @@
-# coding=utf-8
-# This file is created to manage the app configuration
-# Copyright: nephoscale
-# Start Date: 28th Oct 2016
+# -*- coding: utf-8 -*-
+# _______________________________________________________
+# | File Name: exception.py                             |
+# |                                                     |
+# | Package Name: Python-Sidecar REST API               |
+# |                                                     |
+# | Version: 2.0                                        |
+# |                                                     |
+# | Sofatware: Openstack                                |
+# |_____________________________________________________|
+# | Copyright: 2016@nephoscale.com                      |
+# |                                                     |
+# | Author:  info@nephoscale.com                        |
+# |_____________________________________________________|
 
 import logging
-import os
-
 from oslo_config import cfg
 from oslo_log import log
 from paste import deploy
 from pecan import make_app
 import pecan
-
-# Load from current folders
+import os, socket, sys, oslo_i18n
 import model  
-from sidecar import config as api_config
+import config as api_config
 LOG = log.getLogger(__name__)
-
 CONF = cfg.CONF
+OPTS = [
+    cfg.StrOpt('host',
+               default="controller",
+               help='Name of this node, which must be valid in an AMQP '
+               'key. Can be an opaque identifier. For ZeroMQ only, must '
+               'be a valid host name, FQDN, or IP address.'),
+    cfg.IntOpt('http_timeout',
+               default=600,
+               help='Timeout seconds for HTTP requests. Set it to None to '
+                    'disable timeout.'),
+]
+cfg.CONF.register_opts(OPTS)
+
+
+CLI_OPTS = [
+    cfg.StrOpt('os-username',
+               deprecated_group="DEFAULT",
+               default=os.environ.get('OS_USERNAME', 'admin'),
+               help='User name to use for OpenStack service access.'),
+    cfg.StrOpt('os-password',
+               deprecated_group="DEFAULT",
+               secret=True,
+               default=os.environ.get('OS_PASSWORD', 'demo'),
+               help='Password to use for OpenStack service access.'),
+    cfg.StrOpt('os-tenant-id',
+               deprecated_group="DEFAULT",
+               default=os.environ.get('OS_TENANT_ID', '895dd72055d646b8a6af64d441cb6f33'),
+               help='Tenant ID to use for OpenStack service access.'),
+    cfg.StrOpt('os-tenant-name',
+               deprecated_group="DEFAULT",
+               default=os.environ.get('OS_TENANT_NAME', 'admin'),
+               help='Tenant name to use for OpenStack service access.'),
+    cfg.StrOpt('os-cacert',
+               default=os.environ.get('OS_CACERT'),
+               help='Certificate chain for SSL validation.'),
+    cfg.StrOpt('os-auth-url',
+               deprecated_group="DEFAULT",
+               default=os.environ.get('OS_AUTH_URL',
+                                      'http://198.100.181.66:5000/v2.0'),
+               help='Auth URL to use for OpenStack service access.')
+]
+
+cfg.CONF.register_cli_opts(CLI_OPTS, group="service_credentials")
+
+
+# Registering diffrent workers
+API_OPT = cfg.IntOpt('workers',
+                     default=1,
+                     min=1,
+                     deprecated_group='DEFAULT',
+                     deprecated_name='api_workers',
+                     help='Number of workers for api, default value is 1.')
+cfg.CONF.register_opt(API_OPT, 'api')
+
+NOTI_OPT = cfg.IntOpt('workers',
+                      default=1,
+                      min=1,
+                      deprecated_group='DEFAULT',
+                      deprecated_name='notification_workers',
+                      help='Number of workers for notification service, '
+                           'default value is 1.')
+cfg.CONF.register_opt(NOTI_OPT, 'notification')
+
+COLL_OPT = cfg.IntOpt('workers',
+                      default=1,
+                      min=1,
+                      deprecated_group='DEFAULT',
+                      deprecated_name='collector_workers',
+                      help='Number of workers for collector service. '
+                           'default value is 1.')
+cfg.CONF.register_opt(COLL_OPT, 'collector')
+
+LOG = log.getLogger(__name__)
+oslo_i18n.enable_lazy()
+logging.basicConfig(level=logging.INFO)
+log.register_options(cfg.CONF)
+log_levels = (cfg.CONF.default_log_levels + ['stevedore=INFO', 'sidecar=INFO'])
+log.set_defaults(default_log_levels=log_levels)
+argv = []
+cfg.CONF(argv[1:], project='sidecar', validate_default_values=True, version="1.0", default_config_files=None)
+
+
 OPTS = [
     cfg.StrOpt(
         'api_paste_config',
@@ -43,16 +131,14 @@ API_OPTS = [
 CONF.register_opts(OPTS)
 CONF.register_opts(API_OPTS, group='api')
 
-#print cfg.CONF.config_file
-
-#parser = cfg.ConfigParser(cfg.CONF.config_file[0],{})
-#parser.parse()
-#for section , entries in parser.sections.iteritems():
-#if section ==  'database':
-#engine = create_engine(entries['connection'][0], pool_recycle=3600)
-#conn = engine.connect()
-
 def get_pecan_config():
+    """
+    # | Get pecan config files
+    # |
+    # | Arguments: None
+    # |
+    # | Returns None
+    """
     # Set up the pecan configuration
     filename = api_config.__file__.replace('.pyc', '.py')
     return pecan.configuration.conf_from_file(filename)
@@ -135,3 +221,4 @@ class VersionSelectorApplication(object):
 
 def app_factory(global_config, **local_conf):
     return VersionSelectorApplication()
+
